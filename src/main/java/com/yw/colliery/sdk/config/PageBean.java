36f9f7b8180a608031fb43f0b66c1d57c9544f9c
xuzhou <xuzhou-lhq@sinosig.com>
2019-05-05 15:36:09 +0800

    pageHelper配置

--- src/main/java/com/yw/colliery/sdk/config/PageBean.java
+++ src/main/java/com/yw/colliery/sdk/config/PageBean.java
@@ -1,5 +1,8 @@
 package com.yw.colliery.sdk.config;
 
+import com.github.pagehelper.Page;
+
+import java.io.Serializable;
 import java.util.List;
 
 /**
@@ -7,90 +10,79 @@ import java.util.List;
  * @author: xuzhou-lhq
  * @create: 2019-05-05 15:24
  **/
-public class PageBean<T> {
+public class PageBean<T> implements Serializable {
+    private static final long serialVersionUID = 8656597559014685635L;
+    private long total;        //总记录数
+    private List<T> list;    //结果集
+    private int pageNum;    // 第几页
+    private int pageSize;    // 每页记录数
+    private int pages;        // 总页数
+    private int size;        // 当前页的数量 <= pageSize，该属性来自ArrayList的size属性
 
-    // 当前页
-    private Integer currentPage = 1;
-    // 每页显示的总条数
-    private Integer pageSize = 10;
-    // 总条数
-    private Integer totalNum;
-    // 是否有下一页
-    private Integer isMore;
-    // 总页数
-    private Integer totalPage;
-    // 开始索引
-    private Integer startIndex;
-    // 分页结果
-    private List<T> items;
-
-    public PageBean() {
-        super();
+    /**
+     * 包装Page对象，因为直接返回Page对象，在JSON处理以及其他情况下会被当成List来处理，
+     * 而出现一些问题。
+     *
+     * @param list          page结果
+     * @param 
+     */
+    public PageBean(List<T> list) {
+        if (list instanceof Page) {
+            Page<T> page = (Page<T>) list;
+            this.pageNum = page.getPageNum();
+            this.pageSize = page.getPageSize();
+            this.total = page.getTotal();
+            this.pages = page.getPages();
+            this.list = page;
+            this.size = page.size();
+        }
     }
 
-    public PageBean(Integer currentPage, Integer pageSize, Integer totalNum) {
-        super();
-        this.currentPage = currentPage;
-        this.pageSize = pageSize;
-        this.totalNum = totalNum;
-        this.totalPage = (this.totalNum+this.pageSize-1)/this.pageSize;
-        this.startIndex = (this.currentPage-1)*this.pageSize;
-        this.isMore = this.currentPage >= this.totalPage?0:1;
+    public long getTotal() {
+        return total;
     }
 
-    public Integer getCurrentPage() {
-        return currentPage;
+    public void setTotal(long total) {
+        this.total = total;
     }
 
-    public void setCurrentPage(Integer currentPage) {
-        this.currentPage = currentPage;
+    public List<T> getList() {
+        return list;
     }
 
-    public Integer getPageSize() {
+    public void setList(List<T> list) {
+        this.list = list;
+    }
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public int getPageSize() {
         return pageSize;
     }
 
-    public void setPageSize(Integer pageSize) {
+    public void setPageSize(int pageSize) {
         this.pageSize = pageSize;
     }
 
-    public Integer getTotalNum() {
-        return totalNum;
+    public int getPages() {
+        return pages;
     }
 
-    public void setTotalNum(Integer totalNum) {
-        this.totalNum = totalNum;
+    public void setPages(int pages) {
+        this.pages = pages;
     }
 
-    public Integer getIsMore() {
-        return isMore;
+    public int getSize() {
+        return size;
     }
 
-    public void setIsMore(Integer isMore) {
-        this.isMore = isMore;
-    }
-
-    public Integer getTotalPage() {
-        return totalPage;
-    }
-
-    public void setTotalPage(Integer totalPage) {
-        this.totalPage = totalPage;
-    }
-
-    public Integer getStartIndex() {
-        return startIndex;
-    }
-
-    public void setStartIndex(Integer startIndex) {
-        this.startIndex = startIndex;
-    }
-
-    public List<T> getItems() {
-        return items;
-    }
-
-    public void setItems(List<T> items) {
-        this.items = items;
+    public void setSize(int size) {
+        this.size = size;
     }
 }
