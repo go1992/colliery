bdefc6e2a2c7c29800617b228478c21472d7a384
xuzhou <xuzhou-lhq@sinosig.com>
2019-05-05 15:25:46 +0800

    pageHelper配置

---
+++ src/main/java/com/yw/colliery/sdk/config/PageHelperConfig.java
@@ -0,0 +1,28 @@
+package com.yw.colliery.sdk.config;
+
+import com.github.pagehelper.PageHelper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+/**
+ * @description:
+ * @author: xuzhou-lhq
+ * @create: 2019-05-05 15:23
+ **/
+@Configuration
+public class PageHelperConfig {
+
+    @Bean
+    public PageHelper pageHelper(){
+        PageHelper pageHelper = new PageHelper();
+        Properties properties = new Properties();
+        properties.setProperty("offsetAsPageNum","true");
+        properties.setProperty("rowBoundsWithCount","true");
+        properties.setProperty("reasonable","true");
+        properties.setProperty("dialect","mysql");    //配置mysql数据库的方言
+        pageHelper.setProperties(properties);
+        return pageHelper;
+    }
+}
