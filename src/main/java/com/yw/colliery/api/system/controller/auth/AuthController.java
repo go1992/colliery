c03a5a41f207c0bddbdcdf19ae2dedd6db10b2aa
ignorewho <17761256463@163.com>
2019-05-08 00:00:26 +0800

    20190507-提交mapper

--- src/main/java/com/yw/colliery/api/system/controller/auth/AuthController.java
+++ src/main/java/com/yw/colliery/api/system/controller/auth/AuthController.java
@@ -12,6 +12,8 @@ import com.yw.colliery.sdk.utils.LoginSessionUtils;
 import com.yw.colliery.sdk.utils.ResponseUtils;
 import com.yw.colliery.service.auth.AuthService;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -84,14 +86,15 @@ public class AuthController {
             return ResultObject.buildFailResponse("查询权限列表失败!");
         }
     }
-
+    private Logger logger = LoggerFactory.getLogger(AuthController.class);
     @GetMapping("/cover/{authId}")
     public ResultObject coverUserAuth(@PathVariable Integer authId) {
         try {
             UserRelationEntity userRelation = LoginSessionUtils.getUser();
+            logger.info(userRelation.toString() + "  authId=="+ authId);
             if (userRelation != null) {
                 List<AuthEntity> authList = userRelation.getAuthList();
-                boolean result = authList.stream().anyMatch(authEntity -> authEntity.getId().equals(authId));
+                boolean result = authList.stream().anyMatch(authEntity -> authEntity.getId() == authId);
                 return ResultObject.buildSucessResponse(result);
             }
             return ResultObject.buildSucessResponse(false);
