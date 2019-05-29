bb7c9451b5d37f8980461edb8d19be72d81502b6
renzhiqiang <renzhiqiang@haixue.com>
2019-05-29 14:38:10 +0800

    20190528-修改查询接口

--- src/main/java/com/yw/colliery/sdk/aop/auth/AuthModuleAop.java
+++ src/main/java/com/yw/colliery/sdk/aop/auth/AuthModuleAop.java
@@ -10,6 +10,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.util.CollectionUtils;
 
 /**
